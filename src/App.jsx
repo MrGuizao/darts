8226c98c401b433344844891810df8a91c63fe69
MrGuizao <guigen131@gmail.com>
2020-01-27 15:06:29 -0300

    mudando hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,54 @@
 import React, { useState } from "react";
 import "./App.css";
 import Players from "./components/players/Players";
+import MiddleMenu from "./components/menu/MiddleMenu";
 
-const game = {
-     player1: {
-          totalScore: 0,
-          currentScore: 0
-     },
-     player2: {
-          totalScore: 0,
-          currentScore: 0
-     },
-}
-// const game = [
-//      {
+// const game = {
+//      player1: {
 //           totalScore: 0,
 //           currentScore: 0
 //      },
-//      {
+//      player2: {
 //           totalScore: 0,
 //           currentScore: 0
 //      }
-// ];
-
+// };
+const game = [
+     {id: 0, totalScore: 0, currentScore: 0 },
+     {id: 1, totalScore: 0, currentScore: 0 },
+];
 
 function App() {
+     function randomNumber() {
+          return Math.round(Math.random() * 5) + 1;
+     }
+     function lowDice() {
+          return require(`./images/dice-${randomNumber()}.png`);
+     }
+     // function totalScore(total) {
+     //      return (total += randomNumber());
+     // }
+
      const [player, setPlayer] = useState(game);
+     // const [active, setActive] = useState(0);
 
      return (
           <div className="App">
                <h1>BOARD GAME</h1>
+               <MiddleMenu lowDice={lowDice} />
                <main>
-                    <Players number="1" total={player.player1.totalScore} current={player.player1.currentScore} />
-                    <Players number="2" total={player.player2.totalScore} current={player.player2.currentScore} />
+                    <Players
+                         number="1"
+                         total={player.player1.totalScore}
+                         current={player.player1.currentScore}
+                         randomNumber={randomNumber}
+                    />
+                    <Players
+                         number="2"
+                         total={player.player2.totalScore}
+                         current={player.player2.currentScore}
+                         randomNumber={randomNumber}
+                    />
                </main>
           </div>
      );
--- src/components/players/Players.jsx
+++ src/components/players/Players.jsx
@@ -1,25 +1,16 @@
 import React from "react";
 import "./player.css";
 
-import MiddleMenu from "../menu/MiddleMenu";
-
-export default function Players({ number, total, current }) {
-     function randomNumber(){
-          return Math.round(Math.random() * 5) + 1;
-     }
-     function lowDice() {
-          return require(`../../images/dice-${randomNumber()}.png`);
-     }
-     function totalScore(){
-          return total += randomNumber();
-     }
+export default function Players({ number, current, randomNumber }) {
+     // function totalScore(total) {
+     //      return (total += randomNumber());
+     // }
 
      return (
           <>
-               <MiddleMenu lowDice={lowDice} />
                <div className="dashboard-player">
                     <h2>Player {number}</h2>
-                    <h3>Score: {totalScore()}</h3>
+                    <h3>Score: {randomNumber}</h3>
                     <h3>Atual: {current}</h3>
                </div>
           </>
